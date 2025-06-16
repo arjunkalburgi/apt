9004d9f3231558c47ea3fd3c1a58ec9688ed2bb2
Arjun <askalburgi@gmail.com>
2025-06-16 15:15:14 -0400

    Fix ghpages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.VITE_NODE_ENV === 'development';
+
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: adapter({
+			pages: 'build',   // Output directory
+			assets: 'build',
+			fallback: 'index.html',  // Required for SPA routing
+        }),
+		paths: {
+			base: dev ? '' : '/apt',
+		},
+	}
 };
 
 export default config;
